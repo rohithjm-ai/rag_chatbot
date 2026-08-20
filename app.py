9c935a970f3c1720a4493adf1118f18d9359b6f3
Rohith <rohith.jm64@gmail.com>
2026-08-20 10:40:25 +0530

    Add source chunk display to RAG retrieval

--- app.py
+++ app.py
@@ -4,30 +4,34 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_ollama import ChatOllama
 
-pdf_path = r"C:\rag_chatbot\data\iso27001.pdf"
+folder_path = r"C:\rag_chatbot\data\iso27001.pdf"
 splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-loader = PyPDFLoader(pdf_path)
+loader = PyPDFLoader(folder_path)
 documents = loader.load()
 chunks = splitter.split_documents(documents)
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
-db.save_local("faiss_index")
-query = "What is Compatibility with other management system standards?"
-results = db.similarity_search(query, k=3)
-llm = ChatOllama(model="llama3")
+while True:
+    query = input("You : ")
+    if query.lower() == "exit":
+        break
+    results = db.similarity_search(query, k=3)
+    llm = ChatOllama(model="llama3")
 
-context = ""
-for docs in results:
-    context += docs.page_content
-    context += "\n\n"
+    context = ""
+    for i, docs in enumerate(results):
+        print(f"---Source {i + 1}--- ")
+        print(docs.page_content[:300])
+        context += docs.page_content
+        context += "\n\n"
 
-prompt = f"""
-Answer only what we ask 
+    prompt = f"""
+    Answer only what we ask 
 
-context: {context}
+    context: {context}
 
-query : {query}
+    query : {query}
 
-"""
-response = llm.invoke(prompt)
-print(response.content)
+    """
+    response = llm.invoke(prompt)
+    print(response.content)
