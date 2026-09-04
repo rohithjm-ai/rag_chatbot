da3b1e5a6c88750640925fedc9c7b4d18de0ef06
Rohith <rohith.jm64@gmail.com>
2026-09-04 10:12:47 +0530

    Multiple PDFs

--- app.py
+++ app.py
@@ -10,26 +10,29 @@ pdf_files = folder_path.glob("*.pdf")
 splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 documents = []
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+
 for pdf in pdf_files:
     print(f"LOADING : {pdf.name}")
     loader = PyPDFLoader(str(pdf))
     pdf_documents = loader.load()
-    print(f"Size of {len(pdf_documents)}")
+    print(f"Size of {pdf.name} : {len(pdf_documents)}")
     documents.extend(pdf_documents)
 chunks = splitter.split_documents(documents)
 print(f"Total Document : {len(documents)}")
 print(f"Toal Chunks : {len(chunks)}")
+db = FAISS.from_documents(chunks, embeddings)
+db.save_local("faiss_index")
 while True:
     query = input("You : ")
     if query.lower() == "exit":
         break
-    results = db.similarity_search(query, k=3)
     llm = ChatOllama(model="llama3")
-
+    results = db.similarity_search(query, k=3)
     context = ""
     for i, docs in enumerate(results):
         print(f"---Source {i + 1}--- ")
+        source = Path(docs.metadata["source"]).name
+        print(source)
         print(docs.page_content[:300])
         context += docs.page_content
         context += "\n\n"
